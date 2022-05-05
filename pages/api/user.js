d48c90f7333acf60d7830bd5aad587fa84a28d19
NeoDyeBlue <57549644+NeoDyeBlue@users.noreply.github.com>
2022-05-05 07:11:05 +0700

    change some api

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,20 +1,15 @@
 import { getUserBasicInfo, updateUserInfo } from "../../lib/user-queries";
 import { verify } from "jsonwebtoken";
-// const jwt = require("@tsndr/cloudflare-worker-jwt");
 
 export default async function handler(req, res) {
-  // if (req.method !== "GET" || req.method !== "POST")
-  //   return res.status(405).json({ message: "method not allowed" });
   const { cookies } = req;
   const token = cookies.sessionToken;
   const response = {
     authenticated: false,
   };
 
-  // console.log(userId, role);
-
   if (token) {
-    const { userId, role } = verify(token, process.env.JWT_SECRET);
+    const { userId } = verify(token, process.env.JWT_SECRET);
     if (token && userId) {
       if (req.method == "GET") {
         response.authenticated = true;
@@ -29,7 +24,7 @@ export default async function handler(req, res) {
     return res.status(200).json({ ...response });
   }
 
-  res.status(401).json({ message: "auth required" });
+  return res.status(401).json({ ...response });
 }
 
 export const config = {
